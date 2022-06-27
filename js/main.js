2014fbe3bdf3d67a2ed31ccff5ebfa25817dd9e6
andrew-winchell <108092629+andrew-winchell@users.noreply.github.com>
2022-06-27 11:10:07 -0500

    Update

--- js/main.js
+++ js/main.js
@@ -2,25 +2,20 @@ require([
     "esri/portal/Portal",
     "esri/identity/OAuthInfo",
     "esri/identity/IdentityManager",
-    "esri/portal/PortalQueryParams",
-    "esri/config",
-    "esri/Map",
-    "esri/views/MapView",
-    "esri/widgets/BasemapGallery",
-    "esri/widgets/Expand"
-], function (Portal, OAuthInfo, esriId, PortalQueryParams, esriConfig, Map, MapView, BasemapGallery, Expand) {
+    "esri/portal/PortalQueryParams"
+], function (Portal, OAuthInfo, esriId, PortalQueryParams) {
+    //Constants for the HTML div panels
     const personalPanelElement = document.getElementById("personalizedPanel");
     const anonPanelElement = document.getElementById("anonymousPanel");
     const userIdElement = document.getElementById("userId");
 
-
+    //OAuth constant linking to registered AGOL application and logging to Cobec portal
     const INFO = new OAuthInfo({
         appId: "KiHuSotTULGiKtfZ",
         portalUrl: "https://cobecconsulting.maps.arcgis.com",
         authNamespace: "portal_oauth_inline",
         flowtype: "auto",
-        popup: false,
-        //popupCallbackUrl: "oauth-callback.html"
+        popup: false
     });
 
     esriId.registerOAuthInfos([INFO]);
@@ -28,24 +23,74 @@ require([
     esriId.checkSignInStatus(INFO.portalUrl + "/sharing").then(() => {
         displayItems();
     }).catch(() => {
+        //If not signed in, display "sign-in" panel
         anonPanelElement.style.display = "block";
         personalPanelElement.style.display = "none";
     });
-
+    
+    //Listen for click on the "Sign-In" div in html
     document.getElementById("sign-in").addEventListener("click", () => {
         esriId.getCredential(INFO.portalUrl + "/sharing");
     });
-
+    
+    //Listen for click on the "Sign-Out" div in html
     document.getElementById("sign-out").addEventListener("click", () => {
         esriId.destroyCredentials();
         window.location.reload();
     });
 
-    //Developer Dashboard ArcGIS API Key
-    //esriConfig.apiKey = "AAPK0d997cff573e4a00a21f46b5ce9dd0fcDEvU_DalfJdwLqQjyVLb-gt9N5lFcLwfbzVwV9egBdIDWBtpYrgh_I8idfMduczS";
+    function displayItems() {
+      const portal = new Portal();
+      // Setting authMode to immediate signs the user in once loaded
+      portal.authMode = "immediate";
+      // Once loaded, user is signed in
+      portal.load().then(() => {
+        // Create query parameters for the portal search
+        const queryParams = new PortalQueryParams({
+          query: "owner:" + portal.user.username,
+          sortField: "numViews",
+          sortOrder: "desc",
+          num: 20
+        });
+    
+        userIdElement.innerHTML = portal.user.username;
+        anonPanelElement.style.display = "none";
+        personalPanelElement.style.display = "block";
+    
+        // Query the items based on the queryParams created from portal above
+        portal.queryItems(queryParams).then(createGallery);
+      });
+    }
+
+    function createGallery(items) {
+      let htmlFragment = "";
+
+      items.results.forEach((item) => {
+        htmlFragment +=
+          '<div class="esri-item-container">' +
+          (item.thumbnailUrl
+            ? '<div class="esri-image" style="background-image:url(' + item.thumbnailUrl + ');"></div>'
+            : '<div class="esri-image esri-null-image">Thumbnail not available</div>') +
+          (item.title
+            ? '<div class="esri-title">' + (item.title || "") + "</div>"
+            : '<div class="esri-title esri-null-title">Title not available</div>') +
+          "</div>";
+      });
+      document.getElementById("itemGallery").innerHTML = htmlFragment;
+    }
+});
+
+
+require([
+        "esri/config",
+        "esri/Map",
+        "esri/views/MapView",
+        "esri/widgets/BasemapGallery",
+        "esri/widgets/Expand"
+], function (esriConfig, Map, MapView, BasemapGallery, Expand) {
 
     //Initialize the map
-    /*const MAP = new Map({
+    const MAP = new Map({
         basemap: "arcgis-dark-gray"
     });
 
@@ -71,6 +116,5 @@ require([
     //Add Basemap Gallery widget to map view
     VIEW.ui.add([EXPAND], {
         position: "top-right"
-    });*/
-
+    });
 });
