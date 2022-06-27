a929c4e690b74f74d803276561310d24a830aa22
andrew-winchell <108092629+andrew-winchell@users.noreply.github.com>
2022-06-27 16:19:16 -0500

    Update main.js

--- js/main.js
+++ js/main.js
@@ -15,6 +15,25 @@ require([
     const personalPanelElement = document.getElementById("personalizedPanel");
     const anonPanelElement = document.getElementById("anonymousPanel");
     const userIdElement = document.getElementById("userId");
+  
+    //Initialize the map
+    const MAP = new Map({
+    basemap: "arcgis-dark-gray"
+    });
+
+    //Set the map view
+    const VIEW = new MapView({
+        container: "viewDiv",
+        map: MAP,
+        center: [-88, 44],
+        zoom: 4
+    });
+    
+    const MASTERLAYER = new FeatureLayer({
+      // SITREP LAYER url: "https://services3.arcgis.com/rKjecbIat1XHvd9J/arcgis/rest/services/service_dfbfd13d17b54fe4bc253c22e8af0620/FeatureServer"
+      url: "https://services3.arcgis.com/rKjecbIat1XHvd9J/arcgis/rest/services/service_f02b435f02d74f4c94d3dc28796b84f8/FeatureServer",
+      outFields: ["*"]
+    });
 
     //OAuth constant linking to registered AGOL application and logging to Cobec portal
     const INFO = new OAuthInfo({
@@ -28,7 +47,7 @@ require([
     esriId.registerOAuthInfos([INFO]);
     
     esriId.checkSignInStatus(INFO.portalUrl + "/sharing").then(() => {
-        displayMap();
+        displayMap(MAP, VIEW, MASTERLAYER);
     });/*.catch(() => {
         //If not signed in, display "sign-in" panel
         anonPanelElement.style.display = "none";
@@ -38,57 +57,38 @@ require([
     //Get AGOL credentials on startup
     esriId.getCredential(INFO.portalUrl + "/sharing");
 
-    function displayMap() {
-        //Initialize the map
-        const MAP = new Map({
-            basemap: "arcgis-dark-gray"
-        });
-        
+    function displayMap(map, view, fLayer) {
         //Display main app html element
         anonPanelElement.style.display = "none";
         personalPanelElement.style.display = "block";
 
-        addWidgets(MAP);
-        addLayers(MAP);
+        addWidgets(map, view);
+        addLayers(map, fLayer);
     }
 
-    function addWidgets(map) {
-        //Set the map view
-        const VIEW = new MapView({
-            container: "viewDiv",
-            map: map,
-            center: [-88, 44],
-            zoom: 4
-        });
+    function addWidgets(map, view) {
 
         //Initialize Basemap Gallery widget
         const BASEMAPGALLERY = new BasemapGallery({
-            view: VIEW
+            view: view
         });
 
         //Initialize Expand widget
         const EXPAND = new Expand({
-            view: VIEW,
+            view: view,
             content: BASEMAPGALLERY
         });
 
         //Add Basemap Gallery widget to map view
-        VIEW.ui.add([EXPAND], {
+        view.ui.add([EXPAND], {
             position: "top-right"
         });
     }
 
-    function addLayers(map) {
-      const MASTERLAYER = new FeatureLayer({
-        // SITREP LAYER url: "https://services3.arcgis.com/rKjecbIat1XHvd9J/arcgis/rest/services/service_dfbfd13d17b54fe4bc253c22e8af0620/FeatureServer"
-        url: "https://services3.arcgis.com/rKjecbIat1XHvd9J/arcgis/rest/services/service_f02b435f02d74f4c94d3dc28796b84f8/FeatureServer",
-        outFields: ["*"]
-      });
+    function addLayers(map, fLayer) {
 
       //MASTERLAYER.refreshInterval = 0.1;
-      map.add(MASTERLAYER);
-
-      featureDetails(map);
+      map.add(fLayer);
     }
 
     const graphic = {
@@ -97,52 +97,48 @@ require([
       }
     };
 
-    function featureDetails(map) {
-      let view = map.view;
+    const feature = new Feature({
+      graphic: graphic,
+      map: VIEW.map,
+      spatialReference: VIEW.spatialReference
+    });
 
-      const feature = new Feature({
-        graphic: graphic,
-        map: view.map,
-        spatialReference: view.spatialReference
-      });
+    VIEW.ui.add(feature, "bottom-left");
 
-      view.ui.add(feature, "bottom-left");
+    VIEW.whenLayerView().then((layerView) => {
+      let highlight;
+      let objectId;
 
-      view.whenLayerView(fLayer).then((layerView) => {
-        let highlight;
-        let objectId;
-
-        const debouncedUpdate = promiseUtils.debounce((event) => {
-          // Perform a hitTest on the View
-          view.hitTest(event).then((event) => {
-            // Make sure graphic has a popupTemplate
-            const results = event.results.filter((result) => {
-              return result.graphic.layer.popupTemplate;
-            });
-
-            const result = results[0];
-            const newObjectId =
-              result && result.graphic.attributes[fLayer.objectIdField];
-
-            if (!newObjectId) {
-              highlight && highlight.remove();
-              objectId = feature.graphic = null;
-            } else if (objectId !== newObjectId) {
-              highlight && highlight.remove();
-              objectId = newObjectId;
-              feature.graphic = result.graphic;
-              highlight = layerView.highlight(result.graphic);
-            }
+      const debouncedUpdate = promiseUtils.debounce((event) => {
+        // Perform a hitTest on the View
+        VIEW.hitTest(event).then((event) => {
+          // Make sure graphic has a popupTemplate
+          const results = event.results.filter((result) => {
+            return result.graphic.layer.popupTemplate;
           });
-        });
 
-        view.on("pointer-move", (event) => {
-          debouncedUpdate(event).catch((err) => {
-            if (!promiseUtils.isAbortError(err)) {
-              throw err;
-            }
-          });
+          const result = results[0];
+          const newObjectId =
+            result && result.graphic.attributes[fLayer.objectIdField];
+
+          if (!newObjectId) {
+            highlight && highlight.remove();
+            objectId = feature.graphic = null;
+          } else if (objectId !== newObjectId) {
+            highlight && highlight.remove();
+            objectId = newObjectId;
+            feature.graphic = result.graphic;
+            highlight = layerView.highlight(result.graphic);
+          }
         });
       });
-    }
+
+      VIEW.on("pointer-move", (event) => {
+        debouncedUpdate(event).catch((err) => {
+          if (!promiseUtils.isAbortError(err)) {
+            throw err;
+          }
+        });
+      });
+    });
 });
