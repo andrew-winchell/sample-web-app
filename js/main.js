e56ed7553dd0e8e634d7174be3269b63e1e3a0b0
andrew-winchell <108092629+andrew-winchell@users.noreply.github.com>
2022-06-27 14:33:20 -0500

    Update main.js

--- js/main.js
+++ js/main.js
@@ -80,7 +80,9 @@ require([
       const MASTERLAYER = new FeatureLayer({
         // SITREP LAYER url: "https://services3.arcgis.com/rKjecbIat1XHvd9J/arcgis/rest/services/service_dfbfd13d17b54fe4bc253c22e8af0620/FeatureServer"
         url: "https://services3.arcgis.com/rKjecbIat1XHvd9J/arcgis/rest/services/service_f02b435f02d74f4c94d3dc28796b84f8/FeatureServer"
-      }).addTo(map);
+      });
+      
+      map.add(MASTERLAYER);
     }
 
 });
