ff5d1a4292a6e006d2130c6169e3005b0aef0306
andrew-winchell <108092629+andrew-winchell@users.noreply.github.com>
2022-06-30 14:38:06 -0500

    Update main.js

--- js/main.js
+++ js/main.js
@@ -213,7 +213,7 @@ require([
 //function to adjust css properties on side panel open button press
 function openSide() {
   document.getElementById("sidePanel").style.width = "250px";
-  document.getElementById("sidePanel").style.height = 100% - document.getElementById("headerDiv").offsetHeight;
+  document.getElementById("sidePanel").style.height = document.getElementById("headerDiv").offsetHeight;
   document.getElementById("bodyDiv").style.marginLeft = "250px";
   //document.getElementById("headerDiv").style.marginLeft = "250px";
   document.getElementById("footerDiv").style.marginLeft = "250px";
