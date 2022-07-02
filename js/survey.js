453d6368b882bb05366f7673a073df187ef5d2fb
andrew-winchell <108092629+andrew-winchell@users.noreply.github.com>
2022-07-01 20:44:34 -0500

    Update survey.js

--- js/survey.js
+++ js/survey.js
@@ -2,5 +2,5 @@ var newIncidentForm = new Survey123WebForm({
     clientId: "KiHuSotTULGiKtfZ",
     container: "viewDiv",
     itemId: "2f4c44b29a4941dabfa4e401f0f43a53",
-    portalUrl: "https://cobecconsulting.maps.arcgis.com/"
+    portalUrl: "https://cobecconsulting.maps.arcgis.com"
 });
