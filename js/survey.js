c18598418efec8ab620a5e7680e1a08d9f9f7c26
andrew-winchell <108092629+andrew-winchell@users.noreply.github.com>
2022-07-01 22:21:26 -0500

    Update survey.js

--- js/survey.js
+++ js/survey.js
@@ -1,6 +1,49 @@
-var newIncidentForm = new Survey123WebForm({
-    clientId: "KiHuSotTULGiKtfZ",
-    container: "viewDiv",
-    itemId: "2d22a805f44c4f5d8eaa3bc0d7712ca1",
-    portalUrl: "https://cobecconsulting.maps.arcgis.com"
+require([
+    "esri/core/promiseUtils",
+    "esri/identity/OAuthInfo",
+    "esri/identity/IdentityManager",
+    "esri/Map",
+    "esri/views/MapView",
+    "esri/layers/FeatureLayer",
+    "esri/widgets/Feature",
+    "esri/widgets/BasemapGallery",
+    "esri/widgets/Expand"
+], function (promiseUtils, OAuthInfo, esriId, Map, MapView, FeatureLayer, Feature, BasemapGallery, Expand) {
+
+    //OAuth constant linking to registered AGOL application and logging to Cobec portal
+    const info = new OAuthInfo({
+        appId: "KiHuSotTULGiKtfZ",
+        portalUrl: "https://cobecconsulting.maps.arcgis.com",
+        authNamespace: "portal_oauth_inline",
+        flowtype: "auto",
+        popup: false
+    });
+
+    esriId.registerOAuthInfos([info]);
+    
+    //Get AGOL credentials on startup
+    esriId.getCredential(info.portalUrl + "/sharing");
+    
+    esriId.checkSignInStatus(info.portalUrl + "/sharing").then(() => {
+        initializeApp();
+
+    //function to switch displays from the empty anonymous panel to the main app panel with content
+    //called after successful OAuth
+    function initializeApp() {
+        //change html element display styles
+        anonPanelElement.style.display = "none";
+        personalPanelElement.style.display = "block";
+        sidePanelElement.style.display = "block";
+
+        var newIncidentForm = new Survey123WebForm({
+            clientId: "KiHuSotTULGiKtfZ",
+            container: "viewDiv",
+            itemId: "2d22a805f44c4f5d8eaa3bc0d7712ca1",
+            portalUrl: "https://cobecconsulting.maps.arcgis.com"
+        });
+  
+        //call the addWidgets function
+        //addWidgets();
+    }
+    });
 });
