8887f1d2fad4496dcdcf76e476aadf6b054b28a3
andrew-winchell <108092629+andrew-winchell@users.noreply.github.com>
2022-07-01 20:41:49 -0500

    Update survey.js

--- js/survey.js
+++ js/survey.js
@@ -1,4 +1,4 @@
-const newIncidentForm = new Survey123WebForm({
+var newIncidentForm = new Survey123WebForm({
     clientId: "KiHuSotTULGiKtfZ",
     container: "viewDiv",
     itemId: "9d335e842d3f471f97cdba72bd53a430",
