fc7e408e2065902ffc151aa01bdf366691951705
andrew-winchell <108092629+andrew-winchell@users.noreply.github.com>
2022-07-06 15:24:26 -0500

    Update survey.js

--- js/survey.js
+++ js/survey.js
@@ -31,6 +31,7 @@ require([
     //Constants for the HTML elements
     const personalPanelElement = document.getElementById("personalizedPanel");
     const anonPanelElement = document.getElementById("anonymousPanel");
+    const headerPanelElement = document.getElementById("headerDiv");
     const sidePanelElement = document.getElementById("sidePanel");
 
     //function to switch displays from the empty anonymous panel to the main app panel with content
@@ -45,6 +46,69 @@ require([
         //addWidgets();
     }
 
+    //Initialize new FeatureLayer constant
+    const listLayer = new FeatureLayer({
+        //AGOL portal item ID
+        portalItem: {
+          id: "e0e2d6c68a2243b797ab2fd177567d4c"
+        },
+        //if no layerId provided, defaults to first layer in service
+        //layerId: 0
+        popupEnabled: true,
+        outFields: ["*"],
+        //configure popup design
+        popupTemplate: {
+          title: "SITREP: {incident}",
+          content: [
+            {
+              type: "fields",
+              fieldInfos: [
+                {
+                  fieldName: "incident_name",
+                  label: "Incident Name"
+                }
+              ]
+            }
+          ]
+        }
+    });
+
+    //build a query to pull features from feature layer
+    const query = {
+        //if no num or start properties are set, returns the maxRecordCount on feature service
+        //start at the first feature
+        //start: 0,
+        //return first 20 records in query
+        num: 1000,
+        //return all fields from feature
+        outFields: ["*"],
+        //for apps without a map, geometry is unnecessary
+        returnGeometry: true,
+        //order features by {field_name}
+        orderByFields: ["stars_system"]
+      };
+
+      const promise = listLayer.queryFeatures(query).then((featureSet) => convertFeatureSetToRows(featureSet, query));
+      
+      let features;
+      //function to loop through queried feature set and create pick list items for each feature
+      function convertFeatureSetToRows(featureSet, query) {
+        eventsListElement.innerHTML = "";
+        eventsListElement.style.paddingTop = headerPanelElement.style.height.toString();
+
+        features = featureSet.features;
+        features.forEach((result, index) => {
+          const attributes = result.attributes;
+          const name = attributes.tracon_id;
+
+          const item = document.createElement("calcite-pick-list-item");
+          item.setAttribute("label", name);
+          item.setAttribute("value", index);
+          item.setAttribute("description", "test");
+          //item.addEventListener("click", listClickHandler);
+          eventsListElement.appendChild(item);
+        });
+      };
 });
 
 //function to adjust css properties on side panel open button press
